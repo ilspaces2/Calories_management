2b0ecea47464bba738af1b604c674c4562be077f
I_M <moskva.88@mail.ru>
2022-10-25 23:24:30 +0300

    HW3_fix_inmemory_test_create_meals_table.

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,7 +1,6 @@
-DELETE
-FROM user_roles;
-DELETE
-FROM users;
+DELETE FROM user_roles;
+DELETE FROM meals;
+DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -12,3 +11,8 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, description, date_time, calories)
+VALUES (100000,'Breakfast',TIMESTAMP '2011-05-16 15:36:38',1000),
+       (100001,'Dinner',TIMESTAMP '2011-05-16 16:36:38',1500),
+       (100000,'Dinner2',TIMESTAMP '2011-05-16 17:36:38',1500);

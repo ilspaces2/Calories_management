e1bd36df16d9049686ccebc5fe8dbdfeee150a6e
I_M <moskva.88@mail.ru>
2022-11-11 21:56:51 +0300

    6_07_HW5_graph_batch_size

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudUserRepository.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.repository.datajpa;
+import org.springframework.data.jpa.repository.EntityGraph;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,12 +13,12 @@ import ru.javawebinar.topjava.model.User;
 public interface CrudUserRepository extends JpaRepository<User, Integer> {
     @Transactional
     @Modifying
-//    @Query(name = User.DELETE)
     @Query("DELETE FROM User u WHERE u.id=:id")
     int delete(@Param("id") int id);
 
     User getByEmail(String email);
 
-    @Query("SELECT u FROM User u LEFT JOIN FETCH u.meals WHERE u.id = ?1")
+    @EntityGraph(attributePaths = {"meals", "roles"})
+    @Query("SELECT u FROM User u WHERE u.id=?1")
     User getWithMeals(int id);
 }

498d95e0629ac2c2bb386b58eed55c3e1a3dc92c
I_M <moskva.88@mail.ru>
2022-11-15 21:59:50 +0300

    HW6_3

--- src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -1,23 +1,15 @@
 package ru.javawebinar.topjava.web.meal;
 
-import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
-import ru.javawebinar.topjava.to.MealTo;
-import ru.javawebinar.topjava.util.MealsUtil;
-import ru.javawebinar.topjava.web.SecurityUtil;
 
-import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
-import java.util.List;
-import java.util.Objects;
 
 import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
 import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
@@ -30,24 +22,11 @@ public class JspMealController extends AbstractMealController {
         super(service);
     }
 
-    //    @GetMapping("")
-//    public String get(Model model, int id, int userId) {
-//        model.addAttribute("meal", mealService.get(id, userId));
-//        return "index";
-//    }
-
     @GetMapping("/delete")
     public String deleteMeals(@RequestParam int id) {
         super.delete(id);
         return "redirect:/meals";
     }
-
-//    @GetMapping("")
-//    public String getBetweenInclusive(LocalDate startDate, LocalDate endDate, int userId) {
-//        mealService.getBetweenInclusive(startDate, endDate, userId);
-//        return "index";
-//    }
-
 
     @GetMapping("")
     public String getAll(Model model) {
@@ -57,6 +36,7 @@ public class JspMealController extends AbstractMealController {
 
     @GetMapping("/create")
     public String createForm(Model model) {
+        model.addAttribute("action", "create");
         model.addAttribute("meal",
                 new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000));
         return "mealForm";
@@ -68,22 +48,27 @@ public class JspMealController extends AbstractMealController {
         return "mealForm";
     }
 
-    @PostMapping("/exec")
+    @PostMapping("/execute")
     public String createOrUpdate(@ModelAttribute Meal meal) {
-        super.log.info("ENTER");
-        super.create(meal);
+        if (meal.isNew()) {
+            super.create(meal);
+        }
+        super.update(meal, meal.id());
         return "redirect:/meals";
     }
 
     @GetMapping("/filter")
-    public String getMealsBetween(HttpServletRequest request) {
-        LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
-        LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
-        LocalTime startTime = parseLocalTime(request.getParameter("startTime"));
-        LocalTime endTime = parseLocalTime(request.getParameter("endTime"));
-        request.setAttribute("meals", super.getBetween(startDate, startTime, endDate, endTime));
+    public String getMealsBetween(Model model,
+                                  @RequestParam String startDate,
+                                  @RequestParam String endDate,
+                                  @RequestParam String startTime,
+                                  @RequestParam String endTime) {
+        LocalDate startDateParam = parseLocalDate(startDate);
+        LocalDate endDateParam = parseLocalDate(endDate);
+        LocalTime startTimeParam = parseLocalTime(startTime);
+        LocalTime endTimeParam = parseLocalTime(endTime);
+        model.addAttribute("meals", super.getBetween(startDateParam, startTimeParam, endDateParam, endTimeParam));
         return "meals";
     }
-
 }
 

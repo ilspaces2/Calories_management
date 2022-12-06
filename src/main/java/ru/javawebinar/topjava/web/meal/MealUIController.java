6ec1d8dac3466062bfcf8b0ae74656bf528c3089
I_M <moskva.88@mail.ru>
2022-12-06 17:30:26 +0300

    HW8_2_01

--- src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
@@ -1,48 +1,54 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.to.MealTo;
 
+import java.net.URI;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/meals", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/user/meals", produces = MediaType.APPLICATION_JSON_VALUE)
 public class MealUIController extends AbstractMealController {
 
     @Override
-    public Meal get(int id) {
-        return super.get(id);
-    }
-
-    @Override
-    public void delete(int id) {
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
         super.delete(id);
     }
 
     @Override
-    @GetMapping
+    @GetMapping("")
     public List<MealTo> getAll() {
         return super.getAll();
     }
 
-    @Override
-    public Meal create(Meal meal) {
-        return super.create(meal);
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void create(@RequestParam String dateTime,
+                       @RequestParam String description,
+                       @RequestParam int calories) {
+        super.create(new Meal(null, LocalDateTime.parse(dateTime), description, calories));
     }
 
-    @Override
-    public void update(Meal meal, int id) {
-        super.update(meal, id);
-    }
-
-    @Override
-    public List<MealTo> getBetween(LocalDate startDate, LocalTime startTime, LocalDate endDate, LocalTime endTime) {
+    @GetMapping("/filter")
+    public List<MealTo> getBetween(
+            @RequestParam @Nullable LocalDate startDate,
+            @RequestParam @Nullable LocalTime startTime,
+            @RequestParam @Nullable LocalDate endDate,
+            @RequestParam @Nullable LocalTime endTime) {
         return super.getBetween(startDate, startTime, endDate, endTime);
     }
 }
--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -1,8 +1,10 @@
-const mealsAjaxUrl = "meals";
+const mealsAjaxUrl = "user/meals/";
+const mealsFilterAjaxUrl = "user/meals/filter";
 
 // https://stackoverflow.com/a/5064235/548473
 const ctx = {
-    ajaxUrl: mealsAjaxUrl
+    ajaxUrl: mealsAjaxUrl,
+    filterAjaxUrl: mealsFilterAjaxUrl
 };
 
 // $(document).ready(function () {
@@ -33,3 +35,20 @@ $(function () {
         })
     );
 });
+
+function filter() {
+    $.ajax({
+        type: "GET",
+        url: ctx.filterAjaxUrl,
+        data: {
+            startDate: "2020-01-30",
+            endDate: "2020-01-30",
+            startTime: "00:00",
+            endTime: "23:23",
+        }
+    })
+    //TODO
+        // .done(function (data) {
+        // ctx.datatableApi.clear().rows.add(data).draw();
+    // });
+}

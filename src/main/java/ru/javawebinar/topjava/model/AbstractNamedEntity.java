f33c26e313a672db301ac360897c96b5371036f5
I_M <moskva.88@mail.ru>
2022-12-27 23:19:48 +0300

    11_12_XSS

--- src/main/java/ru/javawebinar/topjava/model/AbstractNamedEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractNamedEntity.java
@@ -2,6 +2,8 @@ package ru.javawebinar.topjava.model;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+import ru.javawebinar.topjava.View;
+import ru.javawebinar.topjava.util.validation.NoHtml;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
@@ -11,6 +13,7 @@ public abstract class AbstractNamedEntity extends AbstractBaseEntity {
     @NotBlank
     @Size(min = 2, max = 128)
     @Column(name = "name", nullable = false)
+    @NoHtml(groups = {View.Web.class})
     protected String name;
 
     protected AbstractNamedEntity() {

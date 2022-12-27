7ffbc63873522cda032b7c672728ee43dd9eca9c
I_M <moskva.88@mail.ru>
2022-12-27 14:57:47 +0300

    11_05_HW10_binder_validation

--- src/test/java/ru/javawebinar/topjava/web/user/AdminRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/AdminRestControllerTest.java
@@ -186,7 +186,7 @@ class AdminRestControllerTest extends AbstractControllerTest {
                 .with(userHttpBasic(admin))
                 .content(jsonWithPassword(updated, "password")))
                 .andDo(print())
-                .andExpect(status().isConflict())
+                .andExpect(status().isUnprocessableEntity())
                 .andExpect(errorType(VALIDATION_ERROR))
                 .andExpect(detailMessage(EXCEPTION_DUPLICATE_EMAIL));
     }
@@ -200,7 +200,7 @@ class AdminRestControllerTest extends AbstractControllerTest {
                 .with(userHttpBasic(admin))
                 .content(jsonWithPassword(expected, "newPass")))
                 .andDo(print())
-                .andExpect(status().isConflict())
+                .andExpect(status().isUnprocessableEntity())
                 .andExpect(errorType(VALIDATION_ERROR))
                 .andExpect(detailMessage(EXCEPTION_DUPLICATE_EMAIL));
     }
